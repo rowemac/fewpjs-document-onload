4167b0bcf99cdb97a42796615d5e6c6d3a1c529c
Mac Rowe <m.rowe42@gmail.com>
2021-05-30 09:57:58 -0400

    commit tree 2cfa2d88cd57aa7ffc74aba8908c087a90ac8d7d

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    changeText()
+});
+function changeText() {
+    document.getElementById("text")
+    .innerHTML = "This is really cool!"
+}
